10eaa70ca0b50286747981275b58aa3a1f9b6a22
lvcky-gg <matadorcrab@gmail.com>
2024-08-23 16:49:45 -0400

    n/a

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 use axum::{
-    routing::get,
+    // routing::get,
     Router,
+};
+// use std::net::SocketAddr;
+use tower_http::{
+    services::{ServeDir, ServeFile},
+    // trace::TraceLayer,
 };
 
 #[tokio::main]
 async fn main() {
 
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let serve_dir = ServeDir::new("dist/frontend/browser/")
+        .not_found_service(ServeFile::new("./dist/frontend/browser/index.html"))
+        .append_index_html_on_directories(true);
+
+    let app = Router::new().route_service("/", serve_dir);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
